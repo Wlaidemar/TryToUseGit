ee1e2169ac953a45cd32c80a4006bffb5d7d1dda
Wlaidemar <kuznetsov.wlaidemar.study@gmail.com>
2022-06-21 11:48:35 +0600

    Add project files.

---
+++ TryToUseGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryToUseGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
